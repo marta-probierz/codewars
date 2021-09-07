7370d5667c4bca9b1ebfc9bbc404248cced42727
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-07 07:13:22 +0200

    Update Middle Me.js

--- 7/Middle Me.js
+++ 7/Middle Me.js
@@ -1,3 +1,8 @@
 function middleMe(N, X, Y){
   return N % 2 ? X : Y.repeat(N/2)+X+Y.repeat(N/2);
 }
+
+// second solution
+function middleMe(N, X, Y){
+  return N % 2 === 0 ? Y.repeat(N/2)+X+Y.repeat(N/2) : X;
+}
