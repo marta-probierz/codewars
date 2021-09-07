cb8b7cbd627821130533aa5c3d787727db81bda8
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-07 07:12:01 +0200

    Update Two to One.js

--- 7/Two to One.js
+++ 7/Two to One.js
@@ -2,3 +2,8 @@ function longest(s1, s2) {
   let arr = (s1+s2).split('').sort();
   return [...new Set(arr)].join('');
 }
+
+// shorter
+function longest(s1, s2) {
+  return [...new Set((s1+s2).split('').sort())].join('');
+}
