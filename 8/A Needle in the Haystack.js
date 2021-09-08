f5ebc43d48368188f8bd2c030b8ded2aaceb8766
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-08 11:43:48 +0200

    Update A Needle in the Haystack.js

--- 8/A Needle in the Haystack.js
+++ 8/A Needle in the Haystack.js
@@ -1,3 +1,14 @@
 function findNeedle(haystack) {
   return `found the needle at position ${haystack.indexOf('needle')}`;
 }
+
+// or
+function findNeedle(haystack) {
+  let res = 0;
+  haystack.forEach((e, i) => {
+    if (e === 'needle') {
+      res+=i;
+    }
+  });
+  return `found the needle at position ${res}`;
+}
