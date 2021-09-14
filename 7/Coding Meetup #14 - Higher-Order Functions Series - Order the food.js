ad2b2a21061e80765ccd561daea2c4e2749ad75a
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-14 15:54:15 +0200

    Update Coding Meetup #14 - Higher-Order Functions Series - Order the food.js

--- 7/Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
+++ 7/Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
@@ -4,3 +4,17 @@ function orderFood(list) {
   arr.forEach(function(i) { count[i] = (count[i]||0) + 1;});
   return count;
 }
+
+// or
+function orderFood(list) {
+  let foodOrder = {}
+  
+  for (let i = 0; i <list.length; i++){
+    if (foodOrder[list[i].meal]){
+      foodOrder[list[i].meal]++ 
+    } else {
+      foodOrder[list[i].meal] = 1
+      }
+  }
+  return foodOrder
+}
