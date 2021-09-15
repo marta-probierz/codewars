de3755d9751001ae49ac5e4b0d4680e04c51047f
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-15 13:40:36 +0200

    Update SpeedCode #2 - Array Madness.js

--- 8/SpeedCode #2 - Array Madness.js
+++ 8/SpeedCode #2 - Array Madness.js
@@ -1,3 +1,18 @@
 function arrayMadness(a, b) {
 return Math.floor(a.map(e => Math.pow(e,2)).reduce((tot, i) => tot + i)) > Math.floor(b.map(e => Math.pow(e,3)).reduce((tot, i) => tot + i));
 }
+
+// longer
+function arrayMadness(a, b) {
+    let sum1 = 0;
+    let sum2 = 0;
+    for (let i =0, {length} = a; i < length; ++i)
+    {
+        sum1 += a[i]**2;
+    }
+    for (let i =0, {length} = b; i < length; ++i)
+    {
+        sum2 += b[i]**3;
+    }
+    return sum1 > sum2;
+}
