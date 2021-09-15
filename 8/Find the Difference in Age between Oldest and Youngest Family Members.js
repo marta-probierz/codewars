b12a297b71ad7628e3a2fb9b4dabeb534700fbce
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-15 13:39:30 +0200

    Update Find the Difference in Age between Oldest and Youngest Family Members.js

--- 8/Find the Difference in Age between Oldest and Youngest Family Members.js
+++ 8/Find the Difference in Age between Oldest and Youngest Family Members.js
@@ -1,3 +1,17 @@
 function differenceInAges(ages){
   return [Math.floor(Math.min(...ages)), Math.max(...ages), Math.max(...ages) - Math.floor(Math.min(...ages))];
 }
+
+// longer
+function differenceInAges(ages){
+  let youngest = ages[0];
+  let oldest = ages[0];
+  for(i = 0; i < ages.length; i++) {
+    if(ages[i] > oldest) {
+      oldest = ages[i];
+    } else if(ages[i] < youngest) {
+      youngest = ages[i];
+    }
+  }
+  return [youngest, oldest, oldest - youngest];
+}
