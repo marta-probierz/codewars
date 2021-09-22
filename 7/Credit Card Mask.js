602ca20ead3fd9954d7eb7f2a8267d50d1d6d816
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-22 15:52:21 +0200

    Update Credit Card Mask.js

--- 7/Credit Card Mask.js
+++ 7/Credit Card Mask.js
@@ -1,3 +1,12 @@
 function maskify(cc) {
   return cc.replace(/.(?=.{4})/g, "#");
 }
+
+// longer
+function maskify(cc) {
+  cc = cc.split("");
+  for(let i = 0; i < cc.length - 4; i++){
+    cc[i] = "#";
+  }
+  return cc.join("");
+}
