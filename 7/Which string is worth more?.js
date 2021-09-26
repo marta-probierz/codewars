010ba326ff198495e70d121c83edc5416b053f8d
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-26 14:53:35 +0200

    Update Which string is worth more?.js

--- 7/Which string is worth more?.js
+++ 7/Which string is worth more?.js
@@ -1,1 +1,11 @@
-
+function highestValue(a, b) {
+  let sumA = 0;
+  let sumB = 0;
+  a.split('').forEach(e => {
+    sumA+=e.charCodeAt();
+  });
+  b.split('').forEach(e => {
+    sumB+=e.charCodeAt();
+  });
+  return sumA === sumB ? a : sumA > sumB ? a : b;
+}
