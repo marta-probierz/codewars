7e886670eb545b0d7aa99c84a1697f490b8eb5cb
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-09-27 12:25:49 +0200

    Update Return Two Highest Values in List.js

--- 8/Return Two Highest Values in List.js
+++ 8/Return Two Highest Values in List.js
@@ -1,1 +1,5 @@
-
+function twoHighest(arr) {
+    let arrS = arr.sort((a,b) => a-b);
+    let rem = [...new Set(arrS)];
+    return rem[0] === undefined ? [] : rem[rem.length-2] === undefined ? [rem[rem.length-1]] : [rem[rem.length-1], rem[rem.length-2]];
+}
