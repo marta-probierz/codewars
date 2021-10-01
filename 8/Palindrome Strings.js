692b27dc1f5940047f744e94013b2ed48a6f3900
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-01 07:12:13 +0200

    Update Palindrome Strings.js

--- 8/Palindrome Strings.js
+++ 8/Palindrome Strings.js
@@ -2,3 +2,8 @@ function isPalindrome(line) {
   let s = String(line);
   return s === s.split('').reverse().join('');
 }
+
+// shorter
+function isPalindrome(line) {
+  return String(line) === String(line).split('').reverse().join('');
+}
