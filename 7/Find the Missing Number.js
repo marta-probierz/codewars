d562954ed86731128a80ad8b79db7864fd2451e9
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-03 08:26:59 +0200

    Update Find the Missing Number.js

--- 7/Find the Missing Number.js
+++ 7/Find the Missing Number.js
@@ -1,1 +1,10 @@
-
+function missingNo(n) {
+  let res = []
+  let sor = n.sort((a,b) => a-b);
+  sor.forEach((e, i) => {
+    if (e !== i) {
+      res.push(i)
+    }
+  })
+  return res[0] === undefined ? 100 : res[0];
+}
