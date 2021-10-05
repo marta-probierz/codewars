15abd9bd2a050fd82eec8a06441b375dbc883102
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-05 08:14:36 +0200

    Update Word to binary.js

--- 7/Word to binary.js
+++ 7/Word to binary.js
@@ -1,1 +1,8 @@
-
+function wordToBin(text){
+  let output = [];
+  for (var i = 0;i < text.length; i++) {
+    let bin = text[i].charCodeAt().toString(2);
+    output.push(Array(8-bin.length+1).join("0") + bin);
+  } 
+  return output;
+}
