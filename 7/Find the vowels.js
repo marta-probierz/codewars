459f864c1833de004ec442ac78aad4c6587a6738
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-05 12:35:21 +0200

    Update Find the vowels.js

--- 7/Find the vowels.js
+++ 7/Find the vowels.js
@@ -1,1 +1,10 @@
-
+function vowelIndices(word){
+  word = word.toLowerCase();
+  let res = [];
+  for(let i = 0; i < word.length; i++) {
+    if(word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u' || word[i] === 'y') {
+      res.push(i + 1);
+    }
+  }
+  return res;
+}
