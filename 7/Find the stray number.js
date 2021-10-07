d25fd3f5db318b23a860217743da4edec6fde374
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-07 19:45:37 +0200

    Update Find the stray number.js

--- 7/Find the stray number.js
+++ 7/Find the stray number.js
@@ -1,3 +1,10 @@
 function stray(numbers) {
   return numbers.reduce((a,b) => a^b);
 }
+
+// or
+function stray(numbers) {
+  let max = Math.max(...numbers);
+  let min = Math.min(...numbers);
+  return numbers.filter(x => x == max).length == 1 ? max : min;
+}
