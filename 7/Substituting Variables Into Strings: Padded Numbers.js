b688cf8a10de3cd33247257c9169d95b601fcbd0
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-07 19:55:04 +0200

    Update Substituting Variables Into Strings: Padded Numbers.js

--- 7/Substituting Variables Into Strings: Padded Numbers.js
+++ 7/Substituting Variables Into Strings: Padded Numbers.js
@@ -1,1 +1,7 @@
-
+function solution(value){
+  if (value < 10) return `Value is 0000${value}`;
+  if (value < 100) return `Value is 000${value}`;
+  if (value < 1000) return `Value is 00${value}`;
+  if (value < 10000) return `Value is 0${value}`;
+  if (value < 100000) return `Value is ${value}`;
+}
