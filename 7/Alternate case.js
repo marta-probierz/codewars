ce9dc95350ab947fc805fefe6f5fd817aef78fc9
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-08 12:22:33 +0200

    Update Alternate case.js

--- 7/Alternate case.js
+++ 7/Alternate case.js
@@ -1,1 +1,11 @@
-
+function alternateCase(s) {
+let str = '';
+  for(let i = 0; i < s.length; i++){
+    if (s[i] == s[i].toLowerCase()){ 
+      str += s[i].toUpperCase();
+    } else {
+      str += s[i].toLowerCase();
+    }
+  }
+  return str; 
+}
