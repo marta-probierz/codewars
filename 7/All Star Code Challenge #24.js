1c907add960c30be961eb3277fdcae7dd755316a
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-08 16:08:52 +0200

    Update All Star Code Challenge #24.js

--- 7/All Star Code Challenge #24.js
+++ 7/All Star Code Challenge #24.js
@@ -5,3 +5,7 @@ function hypotenuse(a, b){
 function leg(c, a){
   return Math.sqrt(c*c - a*a);
 }
+
+// shorter
+const hypotenuse = (a,b) => Math.sqrt(a*a + b*b);
+const leg = (c,a) => Math.sqrt(c*c - a*a);
