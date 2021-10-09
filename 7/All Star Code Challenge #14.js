b60e51a18b05f1f7887191560943d09d7048eadd
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-09 18:50:02 +0200

    Update All Star Code Challenge #14.js

--- 7/All Star Code Challenge #14.js
+++ 7/All Star Code Challenge #14.js
@@ -1,1 +1,8 @@
-
+function median(array) {
+  let arr = array.sort((a,b) => a-b);
+  if (arr.length % 2 !== 0) {
+    return arr[Math.floor(arr.length/2)];
+  } else {
+    return (arr[arr.length/2-1] + arr[arr.length/2])/2;
+  }
+}
