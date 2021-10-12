4dde335b0b404f1896cdf776663414c339c2064d
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-12 06:41:32 +0200

    Update Negative Connotation.js

--- 7/Negative Connotation.js
+++ 7/Negative Connotation.js
@@ -1,1 +1,14 @@
-
+// not clever
+function connotation(str) {
+  let firstCount = 0;
+  let secondCount = 0;
+  let arr = str.toLowerCase().split(' ');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === 'a' || arr[i][0] === 'b' || arr[i][0] === 'c' || arr[i][0] === 'd' || arr[i][0] === 'e' || arr[i][0] === 'f' || arr[i][0] === 'g' || arr[i][0] === 'h' || arr[i][0] === 'i' || arr[i][0] === 'j' || arr[i][0] === 'k' || arr[i][0] === 'l' || arr[i][0] === 'm') {
+      firstCount++;
+    } else if (arr[i][0] === 'n' || arr[i][0] === 'o' || arr[i][0] === 'p' || arr[i][0] === 'q' || arr[i][0] === 'r' || arr[i][0] === 's' || arr[i][0] === 't' || arr[i][0] === 'u' || arr[i][0] === 'v' || arr[i][0] === 'w' || arr[i][0] === 'x' || arr[i][0] === 'y' || arr[i][0] === 'z') {
+      secondCount++;
+    }
+  }
+  return firstCount >= secondCount;
+}
