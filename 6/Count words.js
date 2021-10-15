d9c1831170fef4c9700782c1e5c82bae2ff4392d
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-15 08:26:18 +0200

    Update Count words.js

--- 6/Count words.js
+++ 6/Count words.js
@@ -1,1 +1,10 @@
-
+function wordCount(s) {
+  let sum = 0;
+  let arr = s.toLowerCase().replace(/[^a-z ]/g, ' ').replace(/\b(as|the|on|at|of|upon|in|a)\b/g, '').split(' ');  
+  arr.forEach(e => {
+    if (e !== '') {
+      sum++;
+    } 
+  });
+  return sum;
+}
