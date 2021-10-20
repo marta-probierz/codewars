cb98e2bb2e6b4ad2fc48fcc4e74c94c98ba5398f
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-20 10:42:08 +0200

    Update Unique In Order.js

--- 6/Unique In Order.js
+++ 6/Unique In Order.js
@@ -1,1 +1,8 @@
+function uniqueInOrder(iterable){
+  let strArr = Array.isArray(iterable) ? iterable : iterable.split('');
+  let unique = strArr.filter((letter, i) => {
+    return strArr[i] != strArr[i+1];
+  })
 
+  return unique;
+}
