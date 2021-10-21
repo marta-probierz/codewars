f2734f3a5728aa68ee8a3f6044c8cd78d1b7d42f
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-10-21 09:49:59 +0200

    Update Multiples of 3 or 5.js

--- 6/Multiples of 3 or 5.js
+++ 6/Multiples of 3 or 5.js
@@ -1,1 +1,14 @@
-
+function solution(number){
+  if (number <= 0 || number === undefined) {
+    return 0;
+  } else {
+  let arr = [];
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      arr.push(i);
+    }
+  }
+  let setter = [...new Set(arr)];
+  return setter.reduce((tot, i) => tot + i);
+  }
+}
