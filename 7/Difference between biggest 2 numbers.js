7a80cbbc828bee10dab8ba42f7c7eda99ca82fde
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-02 14:09:08 +0100

    Update Difference between biggest 2 numbers.js

--- 7/Difference between biggest 2 numbers.js
+++ 7/Difference between biggest 2 numbers.js
@@ -6,3 +6,11 @@ function diffBig2(arr) {
   let i2 = arr.indexOf(max2);
   return max-max2;
 }
+
+
+// shorter
+const diffBig2 = arr => {
+  let max = Math.max(...arr);
+  arr.splice(arr.indexOf(max), 1);
+  return max - Math.max(...arr);
+}
