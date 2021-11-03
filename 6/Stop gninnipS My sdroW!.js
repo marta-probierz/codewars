1f8525d164ad60efd4995b38afcee89bc53d359f
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-03 15:39:15 +0100

    Update Stop gninnipS My sdroW!.js

--- 6/Stop gninnipS My sdroW!.js
+++ 6/Stop gninnipS My sdroW!.js
@@ -1,3 +1,6 @@
 function spinWords(string){
   return string.split(' ').map(e => e.length >= 5 ? e.split('').reverse().join('') : e).join(' ');
 }
+
+// one line
+const spinWords = string => string.split(' ').map(e => e.length >= 5 ? e.split('').reverse().join('') : e).join(' ');
