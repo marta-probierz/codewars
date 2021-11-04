a9698e5943fcf14f1314fda2e3c3b4914dd346c7
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-04 16:59:25 +0100

    Update Freudian translator.js

--- 8/Freudian translator.js
+++ 8/Freudian translator.js
@@ -1,3 +1,6 @@
 function toFreud(string) {
   return !string ? '' : string.split(' ').map((e) => 'sex').join(' ');
 }
+
+// one line
+const toFreud = string => !string ? '' : string.split(' ').map((e) => 'sex').join(' ');
