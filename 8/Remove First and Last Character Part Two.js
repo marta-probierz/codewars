6d3ea54fd6610d3177ced461ae3fd77fc4423db8
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-07 21:32:51 +0100

    Update Remove First and Last Character Part Two.js

--- 8/Remove First and Last Character Part Two.js
+++ 8/Remove First and Last Character Part Two.js
@@ -4,3 +4,9 @@ function array(arr){
 
 // shorter
 const array = array_ => array_.split(',').slice(1, -1).join(' ') || null
+
+// or
+const array = str => {
+  var arr = str.split(",");
+  return arr.length > 2 ? arr.slice(1,-1).join(" ") : null;
+}
