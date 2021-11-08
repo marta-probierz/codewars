82a0c372667bde930c8b7101e45712c3cd5bc2f1
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-08 14:39:33 +0100

    Update Simple equation reversal.js

--- 7/Simple equation reversal.js
+++ 7/Simple equation reversal.js
@@ -2,3 +2,8 @@ function solve(eq){
   return eq.split('').reverse().join('').replace(/(\d+)/gi,$1=>{
   return $1.split('').reverse().join('')});
 }
+
+// or
+function solve(eq){
+    return eq.split(/\b/g).reverse().join("");
+}
