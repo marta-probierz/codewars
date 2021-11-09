df8f5ba2201b5330e3006081d5a6ab9c46d537c7
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-09 13:40:56 +0100

    Update Fix string case.js

--- 7/Fix string case.js
+++ 7/Fix string case.js
@@ -3,3 +3,6 @@ function solve(s){
     let down=s.split('').filter(v=>v.match(/[a-z]/)).length
     return down>=up?s.toLowerCase():s.toUpperCase();
 }
+
+// one line
+const solve = s => s.replace(/[A-Z]/g,'').length < s.length/2 ? s.toUpperCase() : s.toLowerCase()
