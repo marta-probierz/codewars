a9d4d4a2f401379f42d8eb8c42a9e183b320e049
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-11 20:03:39 +0100

    Update Find Nearest square number.js

--- 8/Find Nearest square number.js
+++ 8/Find Nearest square number.js
@@ -3,3 +3,6 @@ function nearestSq(n){
     for(i=j=n;Math.sqrt(i)%1!=0&&Math.sqrt(j)%1!=0;i++,j--);
       return Math.sqrt(i)%1==0?i:j
 }
+
+// or
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
