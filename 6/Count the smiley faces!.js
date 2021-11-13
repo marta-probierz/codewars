5fb04b47ae39a19d19fb026a20bb8f9cdcf5a275
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-13 12:37:35 +0100

    Update Count the smiley faces!.js

--- 6/Count the smiley faces!.js
+++ 6/Count the smiley faces!.js
@@ -1,3 +1,8 @@
 function countSmileys(arr) {
     return arr.filter(e => e === ':)' || e === ';)' || e === ':-)' || e === ';-)' || e === ':~)' || e === ';~)' || e === ':D' || e === ';D' || e === ':-D' || e === ';-D' || e === ':~D' || e === ';~D').length;
 }
+
+// smarter
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
