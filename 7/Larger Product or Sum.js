a39b4b3c239d56abeaa96f7e43015d241ffbcb9a
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-15 22:47:35 +0100

    Update Larger Product or Sum.js

--- 7/Larger Product or Sum.js
+++ 7/Larger Product or Sum.js
@@ -1,1 +1,6 @@
-
+function sumOrProduct(array, n) {
+  let srt = array.sort((a,b) => a-b);
+  let min = srt.slice(0,n);
+  let max = srt.slice(-n);
+  return max.reduce((e,i) => e+i) > min.reduce((e,i) => e*i) ? 'sum' : max.reduce((e,i) => e+i) == min.reduce((e,i) => e*i) ? 'same' : 'product'
+}
