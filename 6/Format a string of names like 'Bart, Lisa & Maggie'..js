6ce05659be309c164f35459ff060f6f2890624c0
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-18 13:49:04 +0100

    Update Format a string of names like 'Bart, Lisa & Maggie'..js

--- 6/Format a string of names like 'Bart, Lisa & Maggie'..js
+++ 6/Format a string of names like 'Bart, Lisa & Maggie'..js
@@ -1,1 +1,12 @@
+function list(names){
+ if (names.length > 1) {
+   return `${otherNames(names)} & ${names[names.length - 1].name}`
+ } else if (names.length === 1) {
+   return names[0].name
+ }
+  return '';
+}
 
+function otherNames(array) {
+  return array.splice(0, array.length - 1).map(person => person.name).join(', ');
+}
