6fd5910b8d6188904ec081d976b351a0d9ca839e
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-19 12:25:47 +0100

    Update Training JS #2: Basic data types--Number.js

--- 8/Training JS #2: Basic data types--Number.js
+++ 8/Training JS #2: Basic data types--Number.js
@@ -25,3 +25,12 @@ function equal5(){
   var b=  v3 //set number value to b
   return a%b;
 }
+
+// shorter
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+
+const equal1 = () => v1 + v1;
+const equal2 = () => v3 - v1;
+const equal3 = () => v1 * v5;
+const equal4 = () => v4 / v5;
+const equal5 = () => v2 % v4;
