80bae2b68c28b783e47e88fd41df10e8f7c04ca3
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-21 12:29:15 +0100

    Update Training JS #9: loop statement --while and do..while.js

--- 8/Training JS #9: loop statement --while and do..while.js
+++ 8/Training JS #9: loop statement --while and do..while.js
@@ -1,1 +1,15 @@
+const padIt = (string, n) => {
+  let result = string
+  let i = 0
 
+  while (i < n) {
+    if (i % 2 === 0) {
+      result = `*${result}`
+    } else {
+      result = `${result}*`
+    }
+
+    i += 1;
+  }
+  return result;
+}
