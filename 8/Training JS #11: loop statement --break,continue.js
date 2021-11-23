08ccd87224542be682f60e584d63b0268348d67b
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-23 08:20:01 +0100

    Update Training JS #11: loop statement --break,continue.js

--- 8/Training JS #11: loop statement --break,continue.js
+++ 8/Training JS #11: loop statement --break,continue.js
@@ -11,3 +11,14 @@ const grabDoll = dolls => {
 
   return bag
 }
+
+// or
+function grabDoll(dolls){
+  var bag=[];
+  for (d in dolls) {
+     if (dolls[d]=="Hello Kitty"||dolls[d]=="Barbie doll") bag.push(dolls[d]);
+     if (bag.length<3) continue;
+     break;
+  }
+  return bag;
+}
