26ba7d3dfed533bcc8ad1f52d00a82a012fff29b
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-23 09:28:59 +0100

    Update Training JS #12: loop statement --for..in and for..of.js

--- 8/Training JS #12: loop statement --for..in and for..of.js
+++ 8/Training JS #12: loop statement --for..in and for..of.js
@@ -1,1 +1,8 @@
-
+const giveMeFive = object => {
+  let res = []
+  for (let key in object) {
+      if (key.length === 5) res.push(key)
+      if (object[key].length === 5) res.push(object[key]) 
+  }
+  return res;
+}
