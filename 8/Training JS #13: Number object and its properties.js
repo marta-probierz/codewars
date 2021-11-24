b270d4ee6489aefeb480a8965c10e23f8066eced
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-24 09:14:54 +0100

    Update Training JS #13: Number object and its properties.js

--- 8/Training JS #13: Number object and its properties.js
+++ 8/Training JS #13: Number object and its properties.js
@@ -7,3 +7,15 @@ const numberConstant = constant => {
   if (constant === Number.POSITIVE_INFINITY) return 'Number.POSITIVE_INFINITY'
 }
 const whatNumberIsIt = n => `Input number is ${numberConstant(n)}`
+
+// or
+function whatNumberIsIt(n){
+  switch (true) {
+    case (n==Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
+    case (n==Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
+    case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
+    case (n==Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
+    case (isNaN(n)): return "Input number is Number.NaN";
+    default: return "Input number is " + n;
+  }
+}
