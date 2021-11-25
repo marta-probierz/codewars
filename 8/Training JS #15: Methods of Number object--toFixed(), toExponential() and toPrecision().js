f35cd1a8397e85480e9990a9695335482bd7cb10
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-25 14:18:23 +0100

    Update Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision().js

--- 8/Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision().js
+++ 8/Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision().js
@@ -1,1 +1,4 @@
 const howManySmaller = (arr, n) => arr.map(num => Number(num.toFixed(2))).filter(num => num < n).length;
+
+// or
+const howManySmaller = (arr, n) => arr.filter(number => Number(number.toFixed(2)) < n).length;
