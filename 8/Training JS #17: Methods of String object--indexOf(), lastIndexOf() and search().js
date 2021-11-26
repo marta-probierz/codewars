d41a83d80ac9fa80e7e23877adde97b333ac707c
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-26 10:53:02 +0100

    Update Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search().js

--- 8/Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search().js
+++ 8/Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search().js
@@ -1,3 +1,6 @@
 function firstToLast(str,c){
   return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
 }
+
+// one line
+const firstToLast = (str,c) => str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
