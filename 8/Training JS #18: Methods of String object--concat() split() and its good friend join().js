a2ef8605c6423f4ceaaf851be7335220d2421993
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-27 20:28:01 +0100

    Update Training JS #18: Methods of String object--concat() split() and its good friend join().js

--- 8/Training JS #18: Methods of String object--concat() split() and its good friend join().js
+++ 8/Training JS #18: Methods of String object--concat() split() and its good friend join().js
@@ -1,3 +1,6 @@
 function splitAndMerge(string, separator) {
  return string.split(" ").map(index => index.split("").join(separator)).join(" ");
 }
+
+// one line 
+const splitAndMerge = (string, separator) => string.split(" ").map(index => index.split("").join(separator)).join(" ");
