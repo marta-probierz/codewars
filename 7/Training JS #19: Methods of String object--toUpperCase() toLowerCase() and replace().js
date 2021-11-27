df6853f42c6e460a4893e1a00db59631efc886d5
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-11-27 20:35:01 +0100

    Update Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js

--- 7/Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js
+++ 7/Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace().js
@@ -1,3 +1,6 @@
 function alienLanguage(str){
  return str.split(' ').map(e => e.slice(0,-1).toUpperCase() + e.slice(-1).toLowerCase()).join(' ');
 }
+
+// one line
+const alienLanguage = str => str.split(' ').map(e => e.slice(0,-1).toUpperCase() + e.slice(-1).toLowerCase()).join(' ');
