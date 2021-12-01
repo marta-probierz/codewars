81ebe747ac4f40dde1dbfce147466a2b70b12590
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-01 22:25:09 +0100

    Update Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode().js

--- 7/Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode().js
+++ 7/Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode().js
@@ -22,3 +22,14 @@ const answer1="3745";
 const answer2="RILc";
 //question3: He stole the treasure is...
 const answer3="Expired biscuits";
+
+
+// or
+const topSecret = str => str.replace(/[a-z]/gi, c => String.fromCharCode(c.charCodeAt(0) + (/[abc]/i.test(c) ? 23 : -3)))
+
+//question1: The top secret file number is...
+answer1="1850"
+//question2: Super agent's name is...
+answer2="fQT"
+//question3: He stole the treasure is...
+answer3="Train tire"
