d9a6441de363bd5ad9bd2dc26b9403bd34b3af3d
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-02 07:53:03 +0100

    Update Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js

--- 7/Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js
+++ 7/Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js
@@ -1,1 +1,10 @@
+const shuffleIt = (array, ...swaps) => {
+  const newArray = [...array]
 
+  swaps.forEach(([firstIndex, secondIndex]) => {
+    const [firstNumber, secondNumber] = [newArray[firstIndex], newArray[secondIndex]]
+    ;[newArray[firstIndex], newArray[secondIndex]] = [secondNumber, firstNumber]
+  })
+
+  return newArray
+}
