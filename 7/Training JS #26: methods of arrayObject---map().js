3ffeea3a1cf9926b7ba6b35c86ef748146e72efc
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-15 17:00:20 +0100

    Update Training JS #26: methods of arrayObject---map().js

--- 7/Training JS #26: methods of arrayObject---map().js
+++ 7/Training JS #26: methods of arrayObject---map().js
@@ -4,3 +4,8 @@ function isolateIt(arr){
   if (v.length%2!==0){return v.slice(0,v.length/2)+'|'+v.slice((v.length/2)+1)};
   })
 }
+
+// or
+function isolateIt(arr){
+  return arr.map(s=>s.slice(0,s.length/2)+"|"+s.slice(-s.length/2));
+}
