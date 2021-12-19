f3e3e69df951d0a3ef311ef38737424efc79bd8d
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-19 10:44:22 +0100

    Update Training JS #30: methods of arrayObject---reduce() and reduceRight().js

--- 7/Training JS #30: methods of arrayObject---reduce() and reduceRight().js
+++ 7/Training JS #30: methods of arrayObject---reduce() and reduceRight().js
@@ -4,3 +4,8 @@ function tailAndHead(arr){
      m.push(arr[i]%10+Number((arr[i+1]+'')[0]))
    return m.reduce((a,b)=>a*b)
  }
+
+// or
+function tailAndHead(arr){
+  return arr.reduce((a,b,i)=>a*(+((arr[i]+"")[0])+(arr[i-1]%10)))/arr[0];
+}
