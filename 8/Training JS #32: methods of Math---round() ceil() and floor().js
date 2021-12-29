be8e978f2b2242f96f9224f9de6b32981123b4af
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-29 09:17:09 +0100

    Update Training JS #32: methods of Math---round() ceil() and floor().js

--- 8/Training JS #32: methods of Math---round() ceil() and floor().js
+++ 8/Training JS #32: methods of Math---round() ceil() and floor().js
@@ -4,3 +4,9 @@ function roundIt (n) {
         fn = dx < 0 ? Math.ceil : dx > 0 ? Math.floor : Math.round
     return fn(n)
 }
+
+// or
+function roundIt(n){
+  var [a, b] = n.toString().split('.');
+  return a.length > b.length ? Math.floor(n) : a.length === b.length ? Math.round(n) : Math.ceil(n);
+}
