b62ce4dc94a554a8d9e968ec11b3c7bed2684f52
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-29 09:19:27 +0100

    Update Training JS #33: methods of Math---max() min() and abs().js

--- 7/Training JS #33: methods of Math---max() min() and abs().js
+++ 7/Training JS #33: methods of Math---max() min() and abs().js
@@ -1,4 +1,10 @@
 function maxMin(arr1,arr2){
-  const arr=arr2.map((v,i)=>v-arr1[i],0)
-  return [Math.max(...arr.map(v=>Math.abs(v))),Math.min(...arr.map(v=>Math.abs(v)))]
+  const arr=arr2.map((v,i)=>v-arr1[i],0);
+  return [Math.max(...arr.map(v=>Math.abs(v))),Math.min(...arr.map(v=>Math.abs(v)))];
+}
+
+// or
+function maxMin(arr1,arr2){
+  var rs=arr1.map((x,i)=>Math.abs(x-arr2[i]));
+  return [Math.max(...rs),Math.min(...rs)];
 }
