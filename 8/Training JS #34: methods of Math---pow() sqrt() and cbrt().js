abfc54cf3079813b40ffb81f685f4eef37104339
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-12-30 15:45:51 +0100

    Update Training JS #34: methods of Math---pow() sqrt() and cbrt().js

--- 8/Training JS #34: methods of Math---pow() sqrt() and cbrt().js
+++ 8/Training JS #34: methods of Math---pow() sqrt() and cbrt().js
@@ -1,2 +1,7 @@
-const isIntegerCube = n => Number.isInteger(Math.cbrt(n))
-const cutCube = (volume, n) => isIntegerCube(n) && isIntegerCube(volume / n)
+function cutCube(volume,n){
+  return !(Math.cbrt(n) % 1) && !(Math.cbrt(volume / n) % 1);
+}
+
+// or
+const isIntegerCube = n => Number.isInteger(Math.cbrt(n));
+const cutCube = (volume, n) => isIntegerCube(n) && isIntegerCube(volume / n);
