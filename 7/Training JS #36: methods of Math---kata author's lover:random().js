83c21b2f229b08185d7c46be1dd7f20b2b047690
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-01-02 10:31:58 +0100

    Update Training JS #36: methods of Math---kata author's lover:random().js

--- 7/Training JS #36: methods of Math---kata author's lover:random().js
+++ 7/Training JS #36: methods of Math---kata author's lover:random().js
@@ -1,1 +1,12 @@
-
+function rndCode(){
+  let dict1="ABCDEFGHIJKLM"
+  let dict2 = '0123456789'
+  let dict3 = "~!@#$%^&*"
+  let str=''
+  for (let i=1; i<=8;i++){
+    if (i===1||i===2){str+=dict1[~~(dict1.length*Math.random())]}
+    if (i>2&&i<7){str+=dict2[~~(dict2.length*Math.random())]}
+    if (i>6){str+=dict3[~~(dict3.length*Math.random())]}
+  }
+  return str
+}
