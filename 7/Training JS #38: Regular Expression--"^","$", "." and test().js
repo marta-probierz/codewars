b90b774f72144b028e49276b608296d9871b2316
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-01-02 10:33:31 +0100

    Update Training JS #38: Regular Expression--"^","$", "." and test().js

--- 7/Training JS #38: Regular Expression--"^","$", "." and test().js
+++ 7/Training JS #38: Regular Expression--"^","$", "." and test().js
@@ -1,1 +1,8 @@
-
+function findSimilarity(str,word){
+  var regstr=word[0]                             
+          +word.slice(1,-1).replace(/./g,".")  
+          +word.slice(-1);                     
+  var reg1=new RegExp("^"+regstr+"$");
+  return str.split(' ').filter(v=>reg1.test(v)).join(' ')
+  
+}
