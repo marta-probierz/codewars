c330f2d611d5753947895216e77fea23080c400b
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-01-12 10:24:56 +0100

    Update Training JS #42: Regular Expression--( ?: ), ( ?= ) and ( ?! ).js

--- 7/Training JS #42: Regular Expression--( ?: ), ( ?= ) and ( ?! ).js
+++ 7/Training JS #42: Regular Expression--( ?: ), ( ?= ) and ( ?! ).js
@@ -2,3 +2,7 @@ var regex=/(\d)(?=(\d{3})+$)/g;
 function addCommas(money,reg){
   return money.replace(reg,x=>x+",")
 }
+
+// or
+var regex=/(\d)(?=(\d{3})+$)/g
+const addCommas = (money,reg) => money.replace(reg,x=>x+",");
