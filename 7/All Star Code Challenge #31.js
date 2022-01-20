56cb3e93526afb527c22c964eef0a057cfdf5156
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-01-20 10:06:59 +0100

    Update All Star Code Challenge #31.js

--- 7/All Star Code Challenge #31.js
+++ 7/All Star Code Challenge #31.js
@@ -3,3 +3,8 @@ function helpJesse(array){
   array.map(v=>arr.push(`Pour ${v.amount} mL of ${v.solution} into a ${v.instrument}${v.note?` (${v.note})`:''}`))
   return arr
 }
+
+// or
+function helpJesse(array){
+  return array.map(a => "Pour " + a.amount + " mL of " + a.solution + " into a " + a.instrument + (a.hasOwnProperty("note") ? (" ("+a.note+")") : ""));
+}
