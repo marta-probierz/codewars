02dc698255579086c5689d06e1c2e47f10915fec
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-02-04 10:52:15 +0100

    Update All Star Code Challenge #30.js

--- 7/All Star Code Challenge #30.js
+++ 7/All Star Code Challenge #30.js
@@ -1,1 +1,13 @@
-
+function Member(name,gender,status){
+  this.name = name;
+  this.gender=gender;
+  this.status=status;
+}
+var manCave = function(array){
+  
+  if (array.some(v=>v.name==='Raj')){
+    array=array.map((v,i,arr)=>{
+    return Object.assign(v,{status: 'Single' })}).filter(v=>v.gender!=='female')
+  }
+  return array
+}
