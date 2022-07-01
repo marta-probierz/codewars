ea4795ddfc613293d81edffa98f9bd85da79043c
Marta Probierz <marta.anna.probierz@gmail.com>
2022-07-01 11:55:44 +0200

    ref

--- 6/Are they the "same"?.js
+++ 6/Are they the "same"?.js
@@ -1,11 +1,11 @@
-function comp(array1, array2){
-  if (array1 === null || array2 === null) {
-    return false;
-  } else {
-  let first = array1.map(e => Math.pow(e,2));
-  let theSame = first.filter(value => array2.includes(value));
-  let arr1 = first.sort((a,b) => a-b);
-  let arr2 = array2.sort((a,b) => a-b);
-  return JSON.stringify(arr1) == JSON.stringify(arr2);
-  }
+function comp(array1, array2) {
+	if (array1 === null || array2 === null) {
+		return false;
+	} else {
+		let first = array1.map((e) => Math.pow(e, 2));
+		let theSame = first.filter((value) => array2.includes(value));
+		let arr1 = first.sort((a, b) => a - b);
+		let arr2 = array2.sort((a, b) => a - b);
+		return JSON.stringify(arr1) == JSON.stringify(arr2);
+	}
 }
